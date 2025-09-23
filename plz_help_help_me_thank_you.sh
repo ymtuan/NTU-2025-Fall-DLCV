a4d6de0fbd316caebbd0ca34b246d4273b85ee98
oak server <evantuan05@gmail.com>
2025-09-23 17:57:21 +0800

    tesing script good

--- plz_help_help_me_thank_you.sh
+++ plz_help_help_me_thank_you.sh
@@ -6,10 +6,10 @@ pip3 install -r extra_requirements.txt
 chmod +x get_dataset.sh hw1_download_ckpt.sh hw1_1.sh hw1_2.sh
 
 bash get_dataset.sh || { echo "Dataset download failed"; exit 1; }
-unzip -o -q dataset.zip
+unzip -o -q data_2025.zip
 
+mkdir checkpoints
 bash hw1_download_ckpt.sh || { echo "Checkpoint download failed"; exit 1; }
-
 
 mkdir -p results/p1
 mkdir -p results/p2
@@ -20,4 +20,4 @@ python3 p1_acc.py results/p1/p1_val_pred.csv data_2025/p1_data/office/val.csv
 bash hw1_2.sh data_2025/p2_data/validation/ results/p2
 python3 mean_iou_evaluate.py -g data_2025/p2_data/validation/ -p results/p2/
 
-echo "All inference and evaluation completed. Check the results/ folder."
+echo "All inference and evaluation completed. Check the results/ directory."
--- src/p2/Pytorch-UNet/check.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from PIL import Image
-
-mask = np.array(Image.open("../../../data_2025/p2_data/train/masks/1970_mask.png").convert("RGB"))
-print(np.unique(mask.reshape(-1, 3), axis=0))
