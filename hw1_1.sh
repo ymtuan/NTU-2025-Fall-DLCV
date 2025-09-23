32f743c00045d57917e5190fd116ad1d0cf9cd2e
dandelion <evantuan05@gmail.com>
2025-09-23 16:25:13 +0800

    fix hw1_1.sh

--- hw1_1.sh
+++ hw1_1.sh
@@ -3,4 +3,4 @@
 # TODO - run your inference Python3 code
 MODEL_PATH="checkpoints/p1.pth"
 
-pyhton3 src/p1/inference.py "$1" "$2" "$3" "$MODEL_PATH"
+python3 src/p1/inference.py "$1" "$2" "$3" "$MODEL_PATH"
